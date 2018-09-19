5edf9ee53cff618a7359b100e8b566a70cab40c8
ching75987598 <ching75987598@gmail.com>
2018-09-19 17:29:43 +0800

    data frame

--- main.r
+++ main.r
@@ -2,3 +2,10 @@ cat("THis is the Program,\n for showing Soccer trainning course \n from CGM")
 
 path.csv="exam.csv"
 cat("\npath.csv=",path.csv,"\n")
+
+csv=read.csv(path.csv)
+cat("\nthe csv object is a ",class(csv),"\n")
+cat("having ",ncol(csv)," columns and ",nrow(csv),"rows...\n")
+cat("The column names of csv are below:\n")
+cat(colnames(csv),sep="\n")
+
