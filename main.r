75506b7de2d496409ba1e49badd9d1d42ffbdd09
ching75987598 <ching75987598@gmail.com>
2018-09-19 18:22:05 +0800

    first shiny proto

--- main.r
+++ main.r
@@ -1,11 +1,52 @@
-cat("THis is the Program,\n for showing Soccer trainning course \n from CGM")
+library(shiny)
 
-path.csv="exam.csv"
-cat("\npath.csv=",path.csv,"\n")
+csv=read.csv("exam.csv")
+csv=csv[,c(1:6)]
 
-csv=read.csv(path.csv)
-cat("\nthe csv object is a ",class(csv),"\n")
-cat("having ",ncol(csv)," columns and ",nrow(csv),"rows...\n")
-cat("The column names of csv are below:\n")
-cat(colnames(csv),sep="\n")
+
+
+# Define UI for dataset viewer app ----
+ui=fluidPage(
+    sidebarLayout(
+        sidebarPanel(    
+            selectInput(inputId = "sltLevel",
+                label = "Choose the level:",
+                choices = unique(as.character(csv[,3]))
+            ),
+            selectInput(inputId = "sltClass",
+                label = "Choose the Class:",
+                choices = unique(as.character(csv[,2]))
+            ),
+            numericInput(inputId = "obs",
+                label = "Number of observations to view:",
+                value = 10)
+        ),
+        mainPanel(
+            tableOutput("view")
+        )
+    )
+)
+
+server=function(input, output) {
+  # Return the requested dataset ----
+  f.subCsv=reactive({
+	str.level=input$sltLevel
+	str.class=input$sltClass
+		
+	idx.level=which(csv[,3]==str.level)
+	idx.class=which(csv[,2]==str.class)
+	
+	idx=intersect(idx.level,idx.class)
+	return(csv[idx,])
+  })
+ 
+
+  # Show the first "n" observations ----
+  output$view=renderTable({
+	csv.sub=f.subCsv();
+    head(csv.sub, n =input$obs)
+  })
+}
+shinyApp(ui = ui, server = server)
+
 
